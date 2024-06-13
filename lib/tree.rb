da789ccaa0d4667b9a4ec6c4781d12524bc029a0
SeanMero <MeroFromVero@gmail.com>
2024-06-13 18:17:50 +0800

    Add method to insert new node into tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,23 +2,34 @@
 class Tree
   attr_accessor :root
 
+  def initialize(arr)
+    @root = build_tree(arr)
+  end
+
   def build_tree(arr) # rubocop:disable Metrics/AbcSize
     # sort array and remove duplicates
-    return nil if arr.empty?
     arr.sort!.uniq!
 
-    # identify start, end, and mid
-
+    # base case and then recursion
     return Node.new(arr[0]) if arr.length <= 1
 
-    last = arr.length - 1
-    mid = 0 + last / 2
+    mid = (arr.length - 1) / 2 # rubocop:disable Lint/AmbiguousOperatorPrecedence
 
-    root = Node.new(arr[mid])
-    root.left = build_tree(arr[0..(mid)])
-    root.right = build_tree(arr[(mid + 1)..])
+    root_node = Node.new(arr[mid])
+    root_node.left = build_tree(arr[0..mid])
+    root_node.right = build_tree(arr[(mid + 1)..])
 
     # return the root node
-    root
+    root_node
+  end
+
+  def insert(val, node = root)
+    if val == node.value
+      puts "This value is already in the tree."
+    elsif val < node.value
+      node.left.nil? ? node.left = Node.new(val) : insert(val, node.left)
+    else val > node.value
+      node.right.nil? ? node.right = Node.new(val) : insert(val, node.right)
+    end
   end
 end
--- main.rb
+++ main.rb
@@ -2,5 +2,10 @@ require_relative "lib/node"
 require_relative "lib/tree"
 require "pry-byebug"
 
-test = Tree.new
-test.build_tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+a = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+puts a.root
+puts a.root.value
+puts a.root.left
+puts a.root.right
+a.insert(5)
+a.insert(100)
